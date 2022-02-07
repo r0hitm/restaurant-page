c7f752d62444307d25b1fe26473ac8b49fe99962
Rohit M <63762981+r0hitm@users.noreply.github.com>
2022-02-07 14:26:55 +0530

    Everything complete

--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ import { addContent as addHomePageContent} from './home.js';
 import { addContent as addAboutUs } from './about.js';
 import { addMenu } from './menu.js';
 
+const page = document.getElementById('content');
+
+/**
+ * Clears the page
+ */
+
+const clearPage = () => {
+    page.textContent = '';
+}
+
 // Implementation of tab-switch logic
 const tabs = Array.from(document.querySelectorAll('span.tab'));
 // console.log(tabs);
@@ -16,10 +26,22 @@ tabs.forEach(el => el.addEventListener('click', function() {
     tabs.forEach(el => el.classList.remove('select')); // remove the selection 
     el.classList.add('select'); // and add it to the current one
 
-    // switch (el.textContent) {
-    //     case 'Home':
-    //         addLogo('')
-    // }
+    clearPage();
+    // console.log(el.textContent);
+    switch (el.textContent) {
+        case 'Home':
+            addHomePageContent(page);
+            break;
+        case 'Menu':
+            addMenu(page);
+            break;
+        case 'About':
+            addAboutUs(page);
+            break;
+        default:
+            console.log('An erro occured in index.js, invalid tab value');
+    }
 }));
 
-// addMenu(document.getElementById('content'));
+// initialize the page with home page
+addHomePageContent(page);
